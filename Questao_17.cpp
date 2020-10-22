d872c3b869e83b6ae35d3097ffaeec1bc0d123c4
dimitrilim <dimitrilima09@hotmail.com>
2020-10-22 17:17:31 -0300

    Questao atualizada

--- Questao_17.cpp
+++ Questao_17.cpp
@@ -1,8 +1,8 @@
-//
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void){
-int n, s;
+int n, s, i;
 int v1[n], v2[n], v3[n];
 int *pv1, *pv2, *pv3;
 void soma_vetor(int v1[], int v2[], int v3[], int n);
@@ -10,31 +10,42 @@ void soma_vetor(int v1[], int v2[], int v3[], int n);
 printf("Digite a quantidade de numeros:");
 scanf("%i", &n);
 
-for(int i = 0; i< n; i++){
-    printf("Digite o %i numero do vetor 1: ", i+1);
-        scanf("%i", (v1+i));
+pv1 = (int *)malloc(n* sizeof(int));
+for(i = 0; i< n; i++){
+pv1[i] = i;
 }
 
+for(int i = 0; i< n; i++){
+    printf("Digite o %i numero do vetor 1: ", i+1);
+        scanf("%i", &pv1[i]);
+}
+
+pv2 = (int *)malloc(n * sizeof(int));
+for(i = 0; i< n; i++){
+pv2[i] = i;
+}
 printf("\n");
 
 for(int j = 0; j< n; j++){
     printf("Digite o %i numero do vetor 2: ", j+1);
-        scanf("%i", (v2+j));
+        scanf("%i", &pv2[j]);
 }
 
-pv1 = v1;
-pv2 = v2;
-pv3 = v3;
+pv3 = (int *)malloc(n * sizeof(int));
+for(i = 0; i< n; i++){
+pv3[i] = i;
+}
+
 
 soma_vetor(pv1, pv2, pv3, n );
 
-//for(int k= 0; k< n; k++){
+printf("A soma dos vetores 1 e 2 é:");
 
-printf("A soma dos vetores 1 e 2 é: %i", *pv3 );
-//}
+for(int k= 0; k< n; k++){
+printf("%i ", pv3[k] );
+}
 return 0;
 }
-
 
 
 
